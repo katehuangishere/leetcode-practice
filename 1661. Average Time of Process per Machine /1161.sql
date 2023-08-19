9de5cf70e0872b174514b9f128c75e7d99840980
Kate Huang <102621614+katehuangishere@users.noreply.github.com>
2023-08-19 10:17:35 +0800

    Create 1161.sql

---
+++ 1661. Average Time of Process per Machine /1161.sql
@@ -0,0 +1,5 @@
+SELECT s.machine_id, ROUND(AVG(e.timestamp - s.timestamp), 3) AS processing_time
+FROM Activity s
+LEFT JOIN Activity e ON e.machine_id = s.machine_id 
+WHERE s.activity_type = 'start' AND e.activity_type = 'end'
+GROUP BY s.machine_id;
