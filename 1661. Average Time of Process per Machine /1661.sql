5ea3d1a64b9f66f79971a62a7f6cf19e143488c6
Kate Huang <102621614+katehuangishere@users.noreply.github.com>
2024-11-04 20:44:52 -0500

    Update 1661.sql

--- 1661. Average Time of Process per Machine /1661.sql
+++ 1661. Average Time of Process per Machine /1661.sql
@@ -1,5 +1,5 @@
-SELECT s.machine_id, ROUND(AVG(e.timestamp - s.timestamp), 3) AS processing_time
+SELECT s.machine_id, round(avg(e.timestamp - s.timestamp), 3) AS processing_time
 FROM Activity s
-LEFT JOIN Activity e ON e.machine_id = s.machine_id 
-WHERE s.activity_type = 'start' AND e.activity_type = 'end'
+INNER JOIN Activity e ON s.machine_id = e.machine_id
+WHERE s.activity_type = "start" AND e.activity_type = "end"
 GROUP BY s.machine_id;
