4d986df639a4096d5ea2c6c8625e8331719b44cd
Kate Huang <102621614+katehuangishere@users.noreply.github.com>
2024-11-06 12:34:23 -0500

    Update 1251.sql

--- 1251. Average Selling Price /1251.sql
+++ 1251. Average Selling Price /1251.sql
@@ -1,4 +1,5 @@
-SELECT p.product_id, ROUND(SUM(u.units * p.price) / SUM(u.units), 2) AS average_price
-FROM Prices AS p LEFT JOIN UnitsSold AS u ON p.product_id = u.product_id
-WHERE u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY product_id;
+SELECT p.product_id, ROUND(COALESCE(SUM(p.price * u.units) / SUM(u.units), 0), 2) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u ON p.product_id = u.product_id 
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
