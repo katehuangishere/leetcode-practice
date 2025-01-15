0793f780d34e6b90dfad7280a738c27ac14e51e0
Kate Huang <102621614+katehuangishere@users.noreply.github.com>
2025-01-15 11:21:59 -0500

    Update 1484.sql

--- 1484. Group Sold Products By The Date/1484.sql
+++ 1484. Group Sold Products By The Date/1484.sql
@@ -1,3 +1,7 @@
-SELECT sell_date, COUNT(DISTINCT product) AS num_sold, GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') AS products
+SELECT 
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products  -- GROUP_CONCAT() 專門用於在 GROUP BY 子句中合併多個行的值
 FROM Activities
 GROUP BY sell_date
+ORDER BY sell_date;
