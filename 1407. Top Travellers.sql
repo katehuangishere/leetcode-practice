7e4b8edc6b200033a0706d70e31eb56789af640a
Kate Huang <102621614+katehuangishere@users.noreply.github.com>
2025-01-18 12:39:09 -0500

    Create 1407. Top Travellers.sql

---
+++ 1407. Top Travellers.sql
@@ -0,0 +1,6 @@
+SELECT u.name, 
+       SUM(CASE WHEN r.distance IS NULL THEN 0 ELSE r.distance END) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name;
